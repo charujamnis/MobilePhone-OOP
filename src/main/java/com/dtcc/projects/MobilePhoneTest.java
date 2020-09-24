4a00d83db2f53799a59fb84d8263f6b6c8742d99
charujamnis <charushila.jamnis@gmail.com>
2020-09-24 19:19:45 -0400

    MobilePhoneTest.java changed

--- src/main/java/com/dtcc/projects/MobilePhoneTest.java
+++ src/main/java/com/dtcc/projects/MobilePhoneTest.java
@@ -6,15 +6,19 @@ public class MobilePhoneTest {
         Iphone iphone=new Iphone();
         Samsung samsung=new Samsung();
 
-        System.out.println("Number is "+iphone.getMemory()); //from MobilePhone class...common method.
+        System.out.println("Number is "+iphone.getPhoneNumber());
+        System.out.println("Memory is "+samsung.getMemory()); //from MobilePhone class...common method.
         System.out.println("Carrier Type "+iphone.getCarrierType());
 
-        System.out.println("Iphone FrontCamera: "+iphone.isFrontCamera()); //from Iphone class
+
+        System.out.println("Iphone FrontCamera: "+iphone.isFrontCamera());
+        System.out.println("Iphone BackCamera: "+iphone.isBackCamera());//from Iphone class
         System.out.println("Iphone Ringtone: "+iphone.getRingtone());
         System.out.println("Iphone OS: "+iphone.getOperatingSystem());
 
-        System.out.println("Samsung FrontCamera: "+samsung.isFrontCamera());    //from Samsung class
-        System.out.println("Samsung Ringtone: "+iphone.getRingtone());
+        System.out.println("Samsung FrontCamera: "+samsung.isFrontCamera());
+        System.out.println("Samsung BackCamera: "+samsung.isBackCamera());//from Samsung class
+        System.out.println("Samsung Ringtone: "+samsung.getRingtone());
         System.out.println("Samsung OS: "+samsung.getOperatingSystem());
     }
 }
